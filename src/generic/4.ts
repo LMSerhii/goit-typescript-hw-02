e6f0b6457827355926a040d9ceadf4d0eecf7620
LMSerhii <117682588+LMSerhii@users.noreply.github.com>
2024-03-07 16:17:54 +0200

    generic 4-th - 8-th  tasks

--- src/generic/4.ts
+++ src/generic/4.ts
@@ -1,15 +1,18 @@
 /*
-  Використовуйте generics та інтерфейси, щоб виправити помилку в наступних класах:
+  Використовуйте generics та інтерфейси, щоб виправити помилку 
+  в наступних класах:
 */
 
-class Component {
-  constructor (public props:T) {
-
-  }
+interface ComponenntProps {
+  title: string;
 }
 
-class Page extends Component {
-  pageInfo () {
+class Component<T extends ComponenntProps> {
+  constructor(public props: T) {}
+}
+
+class Page extends Component<ComponenntProps> {
+  pageInfo() {
     console.log(this.props.title);
   }
 }
